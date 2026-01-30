eed88e79a17c2f873661dbdbaf1d0e129b44c60f
dyvan9433-star <dyvan9433@gmail.com>
2026-01-30 16:28:57 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("learnMoreBtn");
+  btn.addEventListener("click", () => {
+    alert("I Love You❤️😒");
+  });
+});
